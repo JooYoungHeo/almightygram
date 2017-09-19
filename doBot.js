2484750e4276e0830dbb7fd2f0a37dc90134cd1d
JooYoungHeo <jyhur900226@naver.com>
2017-09-19 16:02:54 +0900

    [작업] telebot reply msg with mongoose test

--- doBot.js
+++ doBot.js
@@ -1,9 +1,25 @@
 let config = require('./config');
 let Telebot = require('telebot');
 let token = config.telegram.token;
+let path = require('path');
+let {Subway} = require(path.join(process.cwd(), 'models'));
 
 const bot = new Telebot(token);
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+bot.on('text', (msg) => {
+  return find().then(result => {
+    return bot.sendMessage(msg.from.id, JSON.stringify(result));
+  }).catch(err => {
+    return bot.sendMessage(msg.from.id, err);
+  });
+});
+
+function find() {
+  return new Promise((resolve, reject) => {
+    Subway.find().exec((err, item) => {
+      err? reject(err): resolve(item[0].downTrain.timetable);
+    });
+  });
+}
 
 bot.start();
