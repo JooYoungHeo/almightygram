be07d1fb05de2a3497bbe88f0b5bdf4f487c034a
JooYoungHeo <jyhur900226@naver.com>
2017-09-20 22:04:39 +0900

    [작업] test bot (one item)

--- doBot.js
+++ doBot.js
@@ -7,17 +7,24 @@ let {Subway} = require(path.join(process.cwd(), 'models'));
 const bot = new Telebot(token);
 
 bot.on('text', (msg) => {
-  return find().then(result => {
+  return find(msg.text).then(result => {
     return bot.sendMessage(msg.from.id, JSON.stringify(result));
   }).catch(err => {
     return bot.sendMessage(msg.from.id, err);
   });
 });
 
-function find() {
+function find(station) {
   return new Promise((resolve, reject) => {
-    Subway.find().exec((err, item) => {
-      err? reject(err): resolve(item[0].downTrain.timetable);
+    Subway.find({
+      'station': station
+    }).exec((err, item) => {
+      if (err) reject(err);
+      if (item.length === 0) {
+        resolve('정보가 없습니다');
+      } else {
+        resolve(item[0].downTrain.timetable);
+      }
     });
   });
 }
