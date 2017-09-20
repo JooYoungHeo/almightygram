17237a1144d5d82f2cbf11fad6eac4e383053054
JooYoungHeo <jyhur900226@naver.com>
2017-09-20 21:42:15 +0900

    [작업] 첫-막차, 타임테이블, 출구정보, 버스노선정보 파싱 테스트 (one url)

--- parsingSubwayInfo.js
+++ parsingSubwayInfo.js
@@ -51,13 +51,72 @@ request(url, (err, response, body) => {
 
   let $ = cheerio.load(body);
   let tableList = $('table.table');
+  let titleList = $('.box-title');
 
   for (let i = 0 ; i < tableList.length ; i++) {
     let eachTable = tableList.eq(i);
+    let title = titleList.eq(i).text();
+    let tr = eachTable.find('tr');
     let td = eachTable.find('td');
-    console.log(`${i}-th table`);
-    for (let j = 0 ; j < td.length ; j++) {
-      console.log(td.eq(j).text());
+    let flag = judgeTitle(title);
+
+    if (flag === 0) {
+      let startEndObject = parsingStartEnd(tr, td);
+    } else if (flag === 1 || flag === 2) {
+      let timetableObject = parsingTimetable(flag, td);
+    } else if (flag === 3) {
+      let exitInfoObject = parsingInfo(td);
+    } else if (flag === 4) {
+      let busInfoObject = parsingInfo(td);
     }
   }
 });
+
+function judgeTitle(title) {
+  return (title.indexOf('첫차') >= 0)? 0: (title.indexOf('상행') >= 0)? 1:
+          (title.indexOf('하행') >= 0)? 2: (title.indexOf('출구') >= 0)? 3:
+          (title.indexOf('버스노선') >= 0)? 4: 5;
+
+}
+
+function parsingStartEnd(tr, td) {
+  let jsonObject = {up: {first: '', last: ''}, down: {first: '', last: ''}};
+  for (let i = 0 ; i < tr.length - 1 ; i++) {
+    let way = td.eq(3*i).text();
+    let first = td.eq(3*i + 1).text();
+    let last = td.eq(3*i + 2).text();
+
+    if (judgeTitle(way) === 1) {
+      jsonObject.up.first = first;
+      jsonObject.up.last = last;
+    } else if (judgeTitle(way) === 2) {
+      jsonObject.down.first = first;
+      jsonObject.down.last = last;
+    }
+  }
+  return jsonObject;
+}
+
+function parsingTimetable(flagNumber, td) {
+  let jsonObject = (flagNumber === 1)? {up: {}}: {down: {}};
+  for (let i = 0 ; i < td.length / 2 ; i++) {
+    let time = td.eq(i*2).text();
+    let minute = td.eq(i*2 + 1).text();
+    if (flagNumber === 1) {
+      jsonObject.up[time] = minute.split(',');
+    } else {
+      jsonObject.down[time] = minute.split(',');
+    }
+  }
+  return jsonObject;
+}
+
+function parsingInfo(td) {
+  let jsonObject = {};
+  for (let i = 0 ; i < td.length / 2 ; i++) {
+    let num = td.eq(i*2).text();
+    let info = td.eq(i*2 + 1).text();
+    jsonObject[num] = info.split(',');
+  }
+  return jsonObject;
+}
