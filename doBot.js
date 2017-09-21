02feb7d56bce0a21e6be53319b4eafeb50847af8
JooYoungHeo <jyhur900226@naver.com>
2017-09-21 10:10:27 +0900

    [작업] subway bot 1차

--- doBot.js
+++ doBot.js
@@ -7,26 +7,62 @@ let {Subway} = require(path.join(process.cwd(), 'models'));
 const bot = new Telebot(token);
 
 bot.on('text', (msg) => {
+
+
   return find(msg.text).then(result => {
-    return bot.sendMessage(msg.from.id, JSON.stringify(result));
+    let infoList = refineData(result);
+    for (let i in infoList) {
+      bot.sendMessage(msg.from.id, JSON.stringify(infoList[i]));
+    }
+    return;
   }).catch(err => {
     return bot.sendMessage(msg.from.id, err);
   });
 });
 
+bot.start();
+
 function find(station) {
   return new Promise((resolve, reject) => {
     Subway.find({
-      'station': station
+      $or: [{'station': station}, {'station': `${station}역`}]
     }).exec((err, item) => {
       if (err) reject(err);
-      if (item.length === 0) {
-        resolve('정보가 없습니다');
-      } else {
-        resolve(item[0].downTrain.timetable);
-      }
+      resolve(item);
     });
   });
 }
 
-bot.start();
+function refineData(item) {
+  let infoList = [];
+  for (let i in item) {
+    let line = item[i].line;
+    let station = item[i].station;
+    let upTrain = item[i].upTrain.timetable;
+    let downTrain = item[i].downTrain.timetable;
+    let upNextArrival = getTime(upTrain);
+    let downNextArrival = getTime(downTrain);
+    infoList.push(`${line} ${station} / 상행: ${upNextArrival}분후 도착 / 하행: ${downNextArrival}분후 도착`);
+  }
+  return infoList;
+}
+
+function getTime(list) {
+  let date = new Date();
+  let hour = formattingTime(date.getHours());
+  let minute = date.getMinutes();
+  let timeList = list[hour];
+  let nextArraival = '';
+
+  for (let i in timeList) {
+    if (Number(timeList[i]) > minute) {
+      nextArraival = Number(timeList[i]);
+      break;
+    }
+  }
+  return nextArraival - minute;
+}
+
+function formattingTime(d) {
+  return (d < 10)? '0' + d.toString(): d.toString();
+}
