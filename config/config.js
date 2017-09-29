a6b9236ea5d769bedf7daf4bf9b3e44f5e037c67
JooYoungHeo <jyhur900226@naver.com>
2017-09-29 18:36:14 +0900

    [작업] config - naver 추가

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ let path = require('path');
 let resourceFile = path.join(process.cwd(), '../resource.json');
 let resource = JSON.parse(fs.readFileSync(resourceFile));
 
-let config = {telegram: resource.telegram};
+let config = {telegram: resource.telegram, naver: resource.naver};
 
 module.exports = config;
