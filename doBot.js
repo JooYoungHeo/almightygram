ee7e3940693797d28e55058e1464c4ecc9472eb2
JooYoungHeo <jyhur900226@naver.com>
2017-10-06 16:03:26 +0900

    [작업] 섭봇 (one data) 로 테스트 완료

--- doBot.js
+++ doBot.js
@@ -6,6 +6,8 @@ let {Subway} = require(path.join(process.cwd(), 'models'));
 
 const bot = new Telebot(token);
 
+let dayTypeList = ['WD', 'SD', 'HD'];
+
 bot.on('text', (msg) => {
   return find(msg.text).then(result => {
     let infoList = refineData(result);
@@ -36,31 +38,62 @@ function refineData(item) {
   for (let i in item) {
     let line = item[i].line;
     let station = item[i].station;
-    let upTrain = item[i].upTrain.timetable;
-    let downTrain = item[i].downTrain.timetable;
-    let upNextArrival = getTime(upTrain);
-    let downNextArrival = getTime(downTrain);
-    infoList.push(`${line} ${station} / 상행: ${upNextArrival}분후 도착 / 하행: ${downNextArrival}분후 도착`);
+    let timeList = item[i].timeTable;
+    let answer = refineTimeList(timeList);
+
+    infoList.push(`${line} ${station} / ${answer}`);
   }
   return infoList;
 }
 
-function getTime(list) {
+function refineTimeList(timeList) {
+  let day = getDay();
+  let timeObject = timeList[day];
+  let timetable = timeObject[dayTypeList[day]];
+  let directionKeys = Object.keys(timetable);
+  let answerList = [];
+
+  for (let i in directionKeys) {
+    let schedule = timetable[directionKeys[i]];
+    let answer = getTime(schedule);
+    answerList.push(`${directionKeys[i]}:${answer}분 후`);
+  }
+
+  return answerList.join(' / ');
+}
+
+function getTime(schedule) {
   let date = new Date();
   let hour = formattingTime(date.getHours());
-  let minute = date.getMinutes();
-  let timeList = list[hour];
-  let nextArraival = '';
+  let nextHour = formattingTime(date.getHours() + 1);
+  let nowMinute = date.getMinutes();
+  let minutes = schedule[hour];
+  let nextMinutes = schedule[nextHour];
+  let flag = false;
+  let returnValue = 0;
 
-  for (let i in timeList) {
-    if (Number(timeList[i]) > minute) {
-      nextArraival = Number(timeList[i]);
+  for (let i in minutes) {
+    if (Number(minutes[i][0]) > nowMinute) {
+      returnValue = Number(minutes[i][0]) - nowMinute;
+      flag = true;
       break;
     }
   }
-  return nextArraival - minute;
+
+  if (!flag) {
+    returnValue = Number(nextMinutes[0][0]) + 60 - nowMinute;
+  }
+
+  return returnValue;
 }
 
 function formattingTime(d) {
   return (d < 10)? '0' + d.toString(): d.toString();
 }
+
+function getDay() {
+  let day = new Date().getDay();
+  return (day === 0)? 2: (day === 6)? 1: 0;
+}
+
+//todo : 시간값 리턴에 행선지 붙이기, 데이터 파싱 다시하기, 마지막 시간 후 에러처리 추가
