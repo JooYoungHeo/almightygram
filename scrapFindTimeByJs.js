7b2b711ce26abdce270e01c4e636cbff0e5ce850
JooYoungHeo <jyhur900226@naver.com>
2017-10-09 21:13:50 +0900

    [작업] 3호선 전체 역 정보 (from file) 파싱하게끔 변경

--- scrapFindTimeByJs.js
+++ scrapFindTimeByJs.js
@@ -1,46 +1,66 @@
 let cheerio = require('cheerio');
 let request = require('request');
 let path = require('path');
+let fs = require('fs');
 let {Subway} = require(path.join(process.cwd(), 'models'));
-
-let url = 'http://www.findtime.co.kr/3%ED%98%B8%EC%84%A0-%EB%82%A8%EB%B6%80%ED%84%B0%EB%AF%B8%EB%84%90%EC%97%AD-%EC%8B%9C%EA%B0%84%ED%91%9C-%EC%B2%AB%EC%B0%A8%EB%A7%89%EC%B0%A8-%EC%8B%9C%EA%B0%84%ED%8F%AC%ED%95%A8/';
 
 let dayTypeList = ['WD', 'SD', 'HD']; // wd: weekday, sd: saturday, hd: holiday
 let dayList = ['평일', '토요일', '일요일'];
 
-let line = '3호선';
-let station = '남부터미널역';
-let type = 'subway';
-let abbreviation = '남터역';
+let file = fs.readFileSync('3LineHref.txt', 'utf-8');
+let hrefList = file.split('\n');
 
-request(url, (err, res, body) => {
-  let $ = cheerio.load(body);
-  let tables = $('.type01');
-  let timeList = [];
+hrefList.forEach(href => {
+  let line = '3호선';
+  let station = '';
+  let type = 'subway';
+  let abbreviation = '';
 
-  for (let i = 0 ; i < tables.length ; i++) {
-    let table = tables.eq(i);
-    let day = table.prev().text();
-    let dayType = '';
-    let thLength = table.children().eq(0).children().eq(0).children().length;
+  if (href !== '') {
+    let decodeUrl = decodeURIComponent(href);
+    let targetText = decodeUrl.split('/')[3];
+    let textList = targetText.split('-');
 
-    if (thLength !== 3) continue;
-
-    for (let j in dayList) {
-      if (day.indexOf(dayList[j]) < 0) continue;
-
-      dayType = dayTypeList[j];
-
-      let thead = table.children().eq(0).children().eq(0).children();
-      let directions = [thead.eq(0).text(), thead.eq(2).text()];
-      let timetable = table.children().eq(1).children();
-      let timeObject = makeTimeObject(dayType, directions, timetable);
-
-      timeList.push(timeObject);
+    for (let i in textList) {
+      if (textList[i].indexOf('역') >= 0) {
+        station = textList[i];
+        break;
+      }
     }
+    requestUrl(href, line, station, type, abbreviation);
   }
-  createDocuments(line, station, timeList, abbreviation, type);
 });
+
+function requestUrl(url, line, station, type, abbreviation) {
+  request(url, (err, res, body) => {
+    let $ = cheerio.load(body);
+    let tables = $('.type01');
+    let timeList = [];
+
+    for (let i = 0 ; i < tables.length ; i++) {
+      let table = tables.eq(i);
+      let day = table.prev().text();
+      let dayType = '';
+      let thLength = table.children().eq(0).children().eq(0).children().length;
+
+      if (thLength !== 3) continue;
+
+      for (let j in dayList) {
+        if (day.indexOf(dayList[j]) < 0) continue;
+
+        dayType = dayTypeList[j];
+
+        let thead = table.children().eq(0).children().eq(0).children();
+        let directions = [thead.eq(0).text(), thead.eq(2).text()];
+        let timetable = table.children().eq(1).children();
+        let timeObject = makeTimeObject(dayType, directions, timetable);
+
+        timeList.push(timeObject);
+      }
+    }
+    createDocuments(line, station, timeList, abbreviation, type);
+  });
+}
 
 function makeTimeObject(dayType, directions, timetable) {
   let timeObject = {};
@@ -63,6 +83,8 @@ function makeTimeObject(dayType, directions, timetable) {
 function refineMinutes(timeText) {
   let list = timeText.split(' ');
   let refineList = [];
+
+  if (timeText === '-') return refineList;
 
   for (let i = 0 ; i < list.length ; i++) {
     if (i === (list.length - 1)) {
