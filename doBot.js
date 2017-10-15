dd5229a34913b3595222b46a9852ad06f61a892b
JooYoungHeo <jyhur900226@naver.com>
2017-10-15 21:08:56 +0900

    [작업] 바뀐 문서 형태에 따른 수정

--- doBot.js
+++ doBot.js
@@ -2,7 +2,7 @@ let config = require('./config');
 let Telebot = require('telebot');
 let token = config.telegram.token;
 let path = require('path');
-let {Subway} = require(path.join(process.cwd(), 'models'));
+let {StationTimeInfo} = require(path.join(process.cwd(), 'models'));
 
 const bot = new Telebot(token);
 
@@ -12,11 +12,12 @@ bot.on('text', (msg) => {
   return find(msg.text).then(result => {
     let infoList = refineData(result);
     for (let i in infoList) {
-      bot.sendMessage(msg.from.id, JSON.stringify(infoList[i]));
+      bot.sendMessage(msg.from.id, infoList[i]);
     }
     return;
   }).catch(err => {
-    return bot.sendMessage(msg.from.id, err);
+    console.error(err);
+    return bot.sendMessage(msg.from.id, '뭔가 잘못되었다.');
   });
 });
 
@@ -24,85 +25,50 @@ bot.start();
 
 function find(station) {
   return new Promise((resolve, reject) => {
-    Subway.find({
-      $or: [{'station': station},
-            {'station': `${station}역`},
+    StationTimeInfo.find({
+      $or: [{'stationName': station},
+            {'stationName': `${station}역`},
             {'abbreviation': station},
             {'abbreviation': `${station}역`}]
     }).exec((err, item) => {
-      if (err) reject(err);
-      resolve(item);
+      err? reject(err): resolve(item);
     });
   });
 }
 
-function refineData(item) {
+function refineData(items) {
   let infoList = [];
-  for (let i in item) {
-    let line = item[i].line;
-    let station = item[i].station;
-    let timeList = item[i].timeTable;
-    let answer = refineTimeList(timeList);
 
-    infoList.push(`${line} ${station} / ${answer}`);
+  for (let i in items) {
+    let item = items[i];
+    let info = getInfo(item.timeTable);
+    let remainTime = (info.depTime !== undefined)? info.depTime: '';
+    let endStName = (info.endStationName !== undefined)? info.endStationName: '';
+    infoList.push(`${item.line} ${item.stationName} - ${item.upDownType} : ${remainTime} (${endStName})`);
   }
+
   return infoList;
 }
 
-function refineTimeList(timeList) {
-  let day = getDay();
-  let timeObject = timeList[day];
-  let timetable = timeObject[dayTypeList[day]];
-  let directionKeys = Object.keys(timetable);
-  let answerList = [];
-
-  for (let i in directionKeys) {
-    let schedule = timetable[directionKeys[i]];
-    let answer = getTimeAndDest(schedule);
-    let dest = (answer.dest === '-')? '': answer.dest;
-    let mention = (answer.remainTime === '')? '도착정보가 없습니다':
-                  (answer.remainTime === 0)? '곧 도착': `${answer.remainTime}분 후 ${dest}`;
-    answerList.push(`${directionKeys[i]}: ${mention}`);
-  }
-
-  return answerList.join(' / ');
-}
-
-function getTimeAndDest(schedule) {
+function getInfo(timeList) {
   let date = new Date();
   let hour = formattingTime(date.getHours());
-  let nextHour = formattingTime(date.getHours() + 1);
-  let nowMinute = date.getMinutes();
-  let minutes = schedule[hour];
-  let nextMinutes = schedule[nextHour];
-  let flag = false;
-  let remainTime = '';
-  let dest = '-';
+  let minute = formattingTime(date.getMinutes());
+  let second = formattingTime(date.getSeconds());
+  let time = hour + minute + second;
+  let returnValue = {};
 
-  if (minutes === undefined) return {remainTime: remainTime, dest: dest};
-
-  for (let i in minutes) {
-    if (Number(minutes[i][0]) >= nowMinute) {
-      remainTime = Number(minutes[i][0]) - nowMinute;
-      dest = minutes[i][1];
-      flag = true;
+  for (let i in timeList) {
+    let item = timeList[i];
+    if (Number(time) < Number(item.depTime)) {
+      returnValue = item;
       break;
     }
   }
 
-  if (!flag && (nextMinutes !== undefined)) {
-    remainTime = Number(nextMinutes[0][0]) + 60 - nowMinute;
-    dest = nextMinutes[0][1];
-  }
-
-  return {remainTime: remainTime, dest: dest};
+  return returnValue;
 }
 
 function formattingTime(d) {
   return (d < 10)? '0' + d.toString(): d.toString();
-}
-
-function getDay() {
-  let day = new Date().getDay();
-  return (day === 0)? 2: (day === 6)? 1: 0;
 }
